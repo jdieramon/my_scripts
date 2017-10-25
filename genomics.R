97538c52544c1625e91382dcdb7c5387824a382a
Jose V. Die <jodiera@upv.es>
2017-10-25 20:53:45 +0200

    Create genomics.R
    A set of the most common functions that I use during analyses of my genomics data

---
+++ genomics.R
@@ -0,0 +1,28 @@
+## Define function to get the two variants from a heterozigous SNP
+
+snp_variants <- function(dna) {
+    # remove special symbol []
+    dna <- gsub("\\[|\\]", "", dna)
+    
+    # get the position of symbol /
+    pos <-  gregexpr2("/", gsub("\\[|\\]", "", dna))[[1]][1]
+    
+    #var1: base1 + exclude base2
+    var1 <-  paste(substr(dna, 1, pos-1), substr(dna, pos+2, nchar(dna)))
+    
+    # remove empty spaces from the string
+    var1 <- gsub(" ", "", var1)
+    
+    #var2: exclude base1 + base2 
+    var2 = paste(substr(dna, 1, pos-2), substr(dna, pos+1, nchar(dna)))
+    
+    # remove empty spaces from the string
+    var2 <- gsub(" ", "", var2)
+    
+    # report variants
+    variants  <-  list(var1 = var1, var2 = var2)
+    return(variants)
+}
+
+## sequence = "TACCGTCC[G/A]GCCTTC"
+## snp_variants(sequence)
