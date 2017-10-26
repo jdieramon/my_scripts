62a176655168ae3929d2522b84967f3132029273
Jose V. Die <jodiera@upv.es>
2017-10-26 18:35:42 +0200

    Update plotRangeColors.R

--- plotRangeColors.R
+++ plotRangeColors.R
@@ -7,7 +7,6 @@
 ## http://rpubs.com/JoseVDie/322171
 
 # Date = date()
-rm(list=ls())
 
 ## --- Dependencies ------------------------------------------------------------
 library(IRanges)
@@ -20,22 +19,22 @@ library(IRanges)
 ## =============================================================================
 
 ##--- Gen1: PPIB -----------------------------------------------------------------
-PPIB <- IRanges(start=c(15,808,1571,4643,5574, 4806, 5617),
-                end=c(125,921,1664,4827,5696, 4825, 5636), 
-                names=c("exon1", "exon2", "exon3", "exon4", "exon5",
-                        "Forward", "Reverse"))
-PPIB
+# PPIB <- IRanges(start=c(15,808,1571,4643,5574, 4806, 5617),
+#                end=c(125,921,1664,4827,5696, 4825, 5636), 
+#                names=c("exon1", "exon2", "exon3", "exon4", "exon5",
+#                        "Forward", "Reverse"))
+# PPIB
 
 
 ## --- Create a vector that indexes the IRanges object containing the primer ---
-target <- c("Forward", "Reverse")
+# target <- c("Forward", "Reverse")
 
 color_ind <- function(ir, target) {
   val <- which(names(ir) %in% target)
   val
 }
 
-c_ind <- color_ind(PPIB, target)
+# c_ind <- color_ind(PPIB, target)
 
 ## --- Define a new function that reads the IRanges object and color_ind output
 getColor <- function(ir, ind, color1, color2) {
@@ -53,7 +52,7 @@ getColor <- function(ir, ind, color1, color2) {
 
 
 # Call that function. 
-color = getColor(PPIB, c_ind, 1, 2)
+# color = getColor(PPIB, c_ind, 1, 2)
 
 ## --- Modify ´plotRanges´ to color the selected ranges ---
 plotRangesColor <- function(x, xlim = x, main = deparse(substitute(x)),
@@ -75,7 +74,7 @@ plotRangesColor <- function(x, xlim = x, main = deparse(substitute(x)),
 }
 
 ## Plot Iranges object
-plotRangesColor(PPIB, col = color, xlim=c(0,5906), main="PPIB")
+# plotRangesColor(PPIB, col = color, xlim=c(0,5906), main="PPIB")
 
 
 ## =============================================================================
@@ -83,24 +82,24 @@ plotRangesColor(PPIB, col = color, xlim=c(0,5906), main="PPIB")
 ## =============================================================================
 
 ##  ---Gen2: DBNDD2 --------------------------------------------------------------
-DBNDD2 <- IRanges(start=c(1889,2217,3680), 
-                  end=c(2026,2354,3888), 
-                  names = c(paste(rep("exon"),1:3, sep="")))
+# DBNDD2 <- IRanges(start=c(1889,2217,3680), 
+#                  end=c(2026,2354,3888), 
+#                  names = c(paste(rep("exon"),1:3, sep="")))
 
-primer.DBNDD2 = IRanges(start=c(2270,2324), end=c(2289,2343), 
-                        names = c("Forward", "Reverse"))
+# primer.DBNDD2 = IRanges(start=c(2270,2324), end=c(2289,2343), 
+#                        names = c("Forward", "Reverse"))
 
 ## Create a sorted IRanges combaining both objects. Then, the rest of the procedure is similar as Example1.   
-sort_ir <- sort(c(DBNDD2, primer.DBNDD2))
+# sort_ir <- sort(c(DBNDD2, primer.DBNDD2))
 
 ## Indexes for the ranges to color
-c_ind <- color_ind(sort_ir, names(primer.DBNDD2))
+# c_ind <- color_ind(sort_ir, names(primer.DBNDD2))
 
 ## Assign color2 to those indexes
-color = getColor(sort_ir, c_ind, 1, 2)
+# color = getColor(sort_ir, c_ind, 1, 2)
 
 ## Plot the IRAnges
-plotRangesColor(sort_ir, col = color, xlim=c(1500,4100), main="DBNDD2 (AC_000170)")
+# plotRangesColor(sort_ir, col = color, xlim=c(1500,4100), main="DBNDD2 (AC_000170)")
 
 ## SessionInfo
-sessionInfo()
+# sessionInfo()
